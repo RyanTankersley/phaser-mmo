8b560c5d0e0338e26e4d1b8547bc9539f75f7b60
Ryan Tankersley <ryan.tankersley@interworks.com>
2015-12-10 17:12:10 -0600

    Added testing needs

---
+++ spec/ControlsSpec.js
@@ -0,0 +1,13 @@
+describe("Controls", function() {
+    
+});
+
+//Simple example
+// describe("Converter", function() {
+//     var timeConverter = require('../lib/converter');
+// 	it("should work", function() {
+// 		var converter = new timeConverter.TimeConverter();
+// 		converter.logMoment();
+//      expect('blah').toEqual('blah');
+// 	});
+// });
