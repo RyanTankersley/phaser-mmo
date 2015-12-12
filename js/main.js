6b2cc48a31b5780020980ef5c58f264b0fc5a312
Ryan Tankersley <ry.tankersley@gmail.com>
2015-12-11 23:27:04 -0600

    Added sockets to main

--- js/main.js
+++ js/main.js
@@ -6,4 +6,6 @@ game.state.add('GameState', GameState);
 game.state.add('HomeState', HomeState);
 game.state.add('PreloadState', PreloadState);
 game.state.add('BootState', BootState);
+
+var socket = new io();
 game.state.start('BootState');
