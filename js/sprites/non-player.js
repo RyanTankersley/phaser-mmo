1a311982b0e02268991a2188d6d3201ac5773291
Ryan Tankersley <ry.tankersley@gmail.com>
2015-12-11 23:57:55 -0600

    Fixed other player movement craziness!  It's officially an MMO!

--- js/sprites/non-player.js
+++ js/sprites/non-player.js
@@ -3,42 +3,41 @@ function NonPlayer(sprite, name, animator) {
     this.sprite = sprite;
     var self = this;
     this.name = name;
-    var prevLoc = {
-        x: sprite.x,
-        y: sprite.y   
-    };
+    this.actualLocation;
     
     var fuzzyCheck = function(num1, num2, fuzz) {
-        return Math.abs(num1, num2) <= fuzz;    
+        console.log(Math.abs(num1 - num2));
+        return Math.abs(num1 - num2) >= fuzz;    
     };
     
     var speed = 100;
-    // this.update = function(newLoc) {
-    //     var fuzzY = fuzzyCheck(newLoc.y, prevLoc.y, 2);
-    //     var fuzzX = fuzzyCheck(newLoc.x, prevLoc.x);
-    //     var isUp = newLoc.y < prevLoc.y && fuzzY;
-    //     var isDown = newLoc.y > prevLoc.y && fuzzY;
-    //     var isLeft = newLoc.x < prevLoc.x && fuzzX;
-    //     var isRight = newLoc.x > prevLoc.x && fuzzX;
-    //     
-    //     self.sprite.body.velocity.x = isLeft ? -speed : isRight ? speed : 0;
-    //     self.sprite.body.velocity.y = isUp ? -speed : isDown ? speed : 0;
-    //     // if(isLeft && !isUp && !isDown) {
-    //     //     animator.play(animator.left);
-    //     // } else if(isRight && !isUp && !isDown) {
-    //     //     animator.play(animator.right);
-    //     // } else if(isUp) {
-    //     //     animator.play(animator.up);
-    //     // } else if(isDown) {
-    //     //     animator.play(animator.down);
-    //     // }
-    //     prevLoc = newLoc;
-    // }
-    
     this.update = function() {
-        self.sprite.body.velocity.x = Math.floor(Math.random() * 100);
-        self.sprite.body.velocity.y = Math.floor(Math.random() * 100);
+        var prevLoc = {x: self.sprite.x, y: self.sprite.y};
+        
+        console.log(prevLoc);
+        var fuzzY = fuzzyCheck(self.actualLocation.y, prevLoc.y, 3);
+        var fuzzX = fuzzyCheck(self.actualLocation.x, prevLoc.x, 3);
+        var isUp = self.actualLocation.y < prevLoc.y && fuzzY;
+        var isDown = self.actualLocation.y > prevLoc.y && fuzzY;
+        var isLeft = self.actualLocation.x < prevLoc.x && fuzzX;
+        var isRight = self.actualLocation.x > prevLoc.x && fuzzX;
+        var x = isLeft ? -speed : isRight ? speed : 0;
+        var y = isUp ? -speed : isDown ? speed : 0;
+        self.sprite.body.velocity.x = x;
+        self.sprite.body.velocity.y = y;
+        
+        if(y > 0)
+            animator.play(animator.down);
+        else if(y < 0)
+            animator.play(animator.up);
+        else if(x > 0)
+            animator.play(animator.right);
+        else if(x < 0)
+            animator.play(animator.left);
+        else
+            animator.stop();
     };
+    
     this.stop = function(loc) {
         prevLoc = loc;
         animator.stop();
--- js/states/game-state.js
+++ js/states/game-state.js
@@ -3,6 +3,7 @@ var GameState = {
     init: function() {
         this.game.time.advancedTiming = true; //this.game.fps for fps
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.stage.disableVisibilityChange = true;
         this.cursors = this.game.input.keyboard.createCursorKeys();
     },
     
@@ -14,20 +15,15 @@ var GameState = {
         this.guy = this.factory.createPlayer(100, 100, 'purple-guy');
         
         socket.on('location-update', function(data) {
-            var user = null;
-            for(var i = 0; i < self.users.length; i++) {
-                if(self.users.name == data.id) {
-                    user = self.users[i];
-                    i = self.users.length;
-                }
-            };
-            
-            if(user == null) {
+            var user = _.where(self.users, {'name': data.id});
+            if(!user.length) {
                 user = self.factory.createNonPlayer(data.location.x, data.location.y, 'purple-guy', data.id);
+                user.actualLocation = data.location
                 self.users.push(user);
             }
-            user.update(data.location);
-            console.log(data);
+            else {
+                user[0].actualLocation = data.location;
+            }
         });
     },
     //executed multiple times per second
@@ -35,5 +31,8 @@ var GameState = {
         var c = this.cursors;
         var controls = new Controls(c.left.isDown, c.right.isDown, c.up.isDown, c.down.isDown);
         this.guy.update(controls);
+        for(var user in this.users) {
+            this.users[user].update();
+        }
     }
 };
