527a573230e68c09c7f0a65b7ab51a6db86f1519
Ryan Tankersley <ry.tankersley@gmail.com>
2015-12-11 23:28:17 -0600

    Funny place for the game when trying to debug issue

--- js/sprites/non-player.js
+++ js/sprites/non-player.js
@@ -8,28 +8,39 @@ function NonPlayer(sprite, name, animator) {
         y: sprite.y   
     };
     
-    this.update = function(newLoc) {
-        var isUp = newLoc.y < prevLoc.y;
-        var isDown = newLoc.y > prevLoc.y;
-        var isLeft = newLoc.x < prevLoc.x;
-        var isRight = newLoc.x > prevLoc.x;
-        
-        // if(isLeft && !isUp && !isDown) {
-        //     animator.play(animator.left);
-        // } else if(isRight && !isUp && !isDown) {
-        //     animator.play(animator.right);
-        // } else if(isUp) {
-        //     animator.play(animator.up);
-        // } else if(isDown) {
-        //     animator.play(animator.down);
-        // }
-        prevLoc = newLoc;
-        sprite.x = newLoc.x;
-        sprite.y = newLoc.y;
-    }
+    var fuzzyCheck = function(num1, num2, fuzz) {
+        return Math.abs(num1, num2) <= fuzz;    
+    };
     
+    var speed = 100;
+    // this.update = function(newLoc) {
+    //     var fuzzY = fuzzyCheck(newLoc.y, prevLoc.y, 2);
+    //     var fuzzX = fuzzyCheck(newLoc.x, prevLoc.x);
+    //     var isUp = newLoc.y < prevLoc.y && fuzzY;
+    //     var isDown = newLoc.y > prevLoc.y && fuzzY;
+    //     var isLeft = newLoc.x < prevLoc.x && fuzzX;
+    //     var isRight = newLoc.x > prevLoc.x && fuzzX;
+    //     
+    //     self.sprite.body.velocity.x = isLeft ? -speed : isRight ? speed : 0;
+    //     self.sprite.body.velocity.y = isUp ? -speed : isDown ? speed : 0;
+    //     // if(isLeft && !isUp && !isDown) {
+    //     //     animator.play(animator.left);
+    //     // } else if(isRight && !isUp && !isDown) {
+    //     //     animator.play(animator.right);
+    //     // } else if(isUp) {
+    //     //     animator.play(animator.up);
+    //     // } else if(isDown) {
+    //     //     animator.play(animator.down);
+    //     // }
+    //     prevLoc = newLoc;
+    // }
+    
+    this.update = function() {
+        self.sprite.body.velocity.x = Math.floor(Math.random() * 100);
+        self.sprite.body.velocity.y = Math.floor(Math.random() * 100);
+    };
     this.stop = function(loc) {
         prevLoc = loc;
-        //animator.stop();
+        animator.stop();
     }
 }
